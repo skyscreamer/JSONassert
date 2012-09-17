4e798239477c8e081e43caaf1b6e390f65ca87b9
James Shaw <js102@zepler.net>
2012-09-17 21:32:33 +0100

    Add more tests of diagnostics

--- src/test/java/org/skyscreamer/jsonassert/JSONCompareTest.java
+++ src/test/java/org/skyscreamer/jsonassert/JSONCompareTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.skyscreamer.jsonassert.JSONCompare.compareJSON;
 import static org.skyscreamer.jsonassert.JSONCompareMode.LENIENT;
+import static org.skyscreamer.jsonassert.JSONCompareMode.NON_EXTENSIBLE;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -16,6 +17,65 @@ import org.junit.internal.matchers.TypeSafeMatcher;
  * Unit tests for {@code JSONCompare}.
  */
 public class JSONCompareTest {
+    @Test
+    public void succeedsWithEmptyArrays() throws JSONException {
+        assertTrue(compareJSON("[]", "[]", LENIENT).passed());
+    }
+
+    @Test
+    public void reportsArraysOfUnequalLength() throws JSONException {
+        JSONCompareResult result = compareJSON("[4]", "[]", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("[]: Expected 1 values and got 0")));
+    }
+
+    @Test
+    public void reportsArrayMissingExpectedElement() throws JSONException {
+        JSONCompareResult result = compareJSON("[4]", "[7]", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("[]: Expected 4, but not found ; []: Contains 7, but not expected")));
+    }
+
+    @Test
+    public void reportsMismatchedFieldValues() throws JSONException {
+        JSONCompareResult result = compareJSON("{\"id\": 3}", "{\"id\": 5}", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("id\nExpected: 3\n     got: 5\n")));
+    }
+
+    @Test
+    public void reportsUnexpectedArrayWhenExpectingObject() throws JSONException {
+        JSONCompareResult result = compareJSON("{}", "[]", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("\nExpected: a JSON object\n     got: a JSON array\n")));
+    }
+
+    @Test
+    public void reportsUnexpectedObjectWhenExpectingArray() throws JSONException {
+        JSONCompareResult result = compareJSON("[]", "{}", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("\nExpected: a JSON array\n     got: a JSON object\n")));
+    }
+
+    @Test
+    public void reportsUnexpectedNull() throws JSONException {
+        JSONCompareResult result = compareJSON("{\"id\": 3}", "{\"id\": null}", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("id: expected java.lang.Integer, but got null")));
+    }
+
+    @Test
+    public void reportsUnexpectedNonNull() throws JSONException {
+        JSONCompareResult result = compareJSON("{\"id\": null}", "{\"id\": \"abc\"}", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("id: expected null, but got a string")));
+    }
+
+    @Test
+    public void reportsUnexpectedFieldInNonExtensibleMode() throws JSONException {
+        JSONCompareResult result = compareJSON("{}", "{\"id\": 3}", NON_EXTENSIBLE);
+        assertThat(result, failsWithMessage(equalTo("Strict checking failed.  Got but did not expect: id")));
+    }
+
+    @Test
+    public void reportsMismatchedTypes() throws JSONException {
+        JSONCompareResult result = compareJSON("{\"arr\":[]}", "{\"arr\":{}}", LENIENT);
+        assertThat(result, failsWithMessage(equalTo("Values of arr have different types: expected an array, but got an object")));
+    }
+
     @Test
     public void reportsWrongSimpleValueCountInUnorderedArray() throws JSONException {
         JSONCompareResult result = compareJSON("[5, 5]", "[5, 7]", LENIENT);
