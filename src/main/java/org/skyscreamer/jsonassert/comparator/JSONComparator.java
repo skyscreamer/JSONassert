1a6b90f9a32818140a130626818e4572a8950caa
Tamas Balog <picimako@gmail.com>
2017-02-17 19:59:05 +0100

    Add javadoc to a couple of methods

--- src/main/java/org/skyscreamer/jsonassert/comparator/JSONComparator.java
+++ src/main/java/org/skyscreamer/jsonassert/comparator/JSONComparator.java
@@ -13,13 +13,59 @@ import org.skyscreamer.jsonassert.JSONCompareResult;
  */
 public interface JSONComparator {
 
+    /**
+     * Compares two {@link JSONObject}s and returns the result of the comparison in a {@link JSONCompareResult} object.
+     *
+     * @param expected the expected JSON object
+     * @param actual   the actual JSON object
+     * @return the result of the comparison
+     * @throws JSONException
+     */
     JSONCompareResult compareJSON(JSONObject expected, JSONObject actual) throws JSONException;
 
+    /**
+     * Compares two {@link JSONArray}s and returns the result of the comparison in a {@link JSONCompareResult} object.
+     *
+     * @param expected the expected JSON array
+     * @param actual   the actual JSON array
+     * @return the result of the comparison
+     * @throws JSONException
+     */
     JSONCompareResult compareJSON(JSONArray expected, JSONArray actual) throws JSONException;
 
+    /**
+     * Compares two {@link JSONObject}s on the provided path represented by {@code prefix} and
+     * updates the result of the comparison in the {@code result} {@link JSONCompareResult} object.
+     *
+     * @param prefix   the path in the json where the comparison happens
+     * @param expected the expected JSON object
+     * @param actual   the actual JSON object
+     * @param result   stores the actual state of the comparison result
+     * @throws JSONException
+     */
     void compareJSON(String prefix, JSONObject expected, JSONObject actual, JSONCompareResult result) throws JSONException;
 
+    /**
+     * Compares two {@link Object}s on the provided path represented by {@code prefix} and
+     * updates the result of the comparison in the {@code result} {@link JSONCompareResult} object.
+     *
+     * @param prefix        the path in the json where the comparison happens
+     * @param expectedValue the expected value
+     * @param actualValue   the actual value
+     * @param result        stores the actual state of the comparison result
+     * @throws JSONException
+     */
     void compareValues(String prefix, Object expectedValue, Object actualValue, JSONCompareResult result) throws JSONException;
 
+    /**
+     * Compares two {@link JSONArray}s on the provided path represented by {@code prefix} and
+     * updates the result of the comparison in the {@code result} {@link JSONCompareResult} object.
+     *
+     * @param prefix   the path in the json where the comparison happens
+     * @param expected the expected JSON array
+     * @param actual   the actual JSON array
+     * @param result   stores the actual state of the comparison result
+     * @throws JSONException
+     */
     void compareJSONArray(String prefix, JSONArray expected, JSONArray actual, JSONCompareResult result) throws JSONException;
 }
