3126b655aace4092e31fec1f9ad21ab7d4a4e595
11813006huanghao <11813006@mail.sustech.edu.cn>
2021-04-12 16:22:17 +0800

    add homework directory

---
+++ src/test/java/homework/MyTest.java
@@ -0,0 +1,14 @@
+package Homework;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyTest {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public  void test1(){
+        assertEquals(5,7);
+    }
+}
