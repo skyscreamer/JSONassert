88d6e1fbd727b296e4483803115bd5f795a72920
lighting-lamp <75935740+lighting-lamp@users.noreply.github.com>
2021-05-19 21:45:53 +0800

    implement issue #88

--- src/test/java/org/skyscreamer/jsonassert/JSONElementTest.java
+++ src/test/java/org/skyscreamer/jsonassert/JSONElementTest.java
@@ -5,28 +5,28 @@ package org.skyscreamer.jsonassert;
 
 public class JSONElementTest
 {
-    // This test fails
+    //CS304 (manually written) Issue link: https://github.com/skyscreamer/JSONassert/issues/88
     @Test
     public void testArrayCompareDifElement() throws JSONException {
         String actual =   "[{ \"id\" : \"123\", \"courseID\" : \"test\"},{ \"id\" : \"124\", \"courseID\" : \"te\"}] ";
         String expected = "[{ \"ID\" : \"127\", \"courseID\" : \"te\"},{ \"id\" : \"125\", \"courseID\" : \"ted\"}] ";
         JSONKeyAssert.assertNotEquals(expected, actual, false);
     }
-
+    //CS304 (manually written) Issue link: https://github.com/skyscreamer/JSONassert/issues/88
     @Test
     public void testArrayCompareSameElement() throws JSONException {
         String actual =   "[{ \"id\" : \"123\", \"courseID\" : \"test\"},{ \"id\" : \"124\", \"courseID\" : \"te\"}] ";
         String expected = "[{ \"id\" : \"127\", \"courseID\" : \"te\"},{ \"id\" : \"125\", \"courseID\" : \"ted\"}] ";
         JSONKeyAssert.assertEquals(expected, actual, false);
     }
-
+    //CS304 (manually written) Issue link: https://github.com/skyscreamer/JSONassert/issues/88
     @Test
     public void testObjectCompareSameElement() throws JSONException {
         String actual =   "{ \"id\" : \"123\", \"courseID\" : \"test\", \"title\" : \"ask question\", \"content\" : null }  ";
         String expected =  "{ \"id\" : \"1\", \"courseID\" : \"test\", \"title\" : \"answer question\", \"content\" : null }  ";
         JSONKeyAssert.assertEquals(expected, actual, true);
     }
-
+    //CS304 (manually written) Issue link: https://github.com/skyscreamer/JSONassert/issues/88
     @Test
     public void testObjectCompareDifElement() throws JSONException {
         String actual =   "{ \"id\" : \"123\", \"courseID\" : \"test\", \"title\" : \"ask question\", \"content\" : null }  ";
