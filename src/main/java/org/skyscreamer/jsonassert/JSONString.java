0ebf74c01374a285d01947c407d653a0b3336d7c
Zack Brown <zackrbrown@gmail.com>
2022-11-11 16:30:15 -0600

    Moved JSONString out of split package

--- src/main/java/org/skyscreamer/jsonassert/JSONString.java
+++ src/main/java/org/skyscreamer/jsonassert/JSONString.java
@@ -12,7 +12,7 @@
  * limitations under the License.
 */
 
-package org.json;
+package org.skyscreamer.jsonassert;
 
 /**
  * The JSONString interface allows a toJSONString() method so that a class can change
